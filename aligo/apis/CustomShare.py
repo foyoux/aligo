6418f23b396f55c7b0d6bf2fbd6e3336dbec8d86
bob <1478449280@qq.com>
2021-08-16 15:17:48 +0800

    优化 自定义 分享, 向后不兼容

--- aligo/apis/CustomShare.py
+++ aligo/apis/CustomShare.py
@@ -1,7 +1,7 @@
 """自定义分享"""
 import base64
 import json
-from typing import List
+from typing import List, Dict
 
 from aligo.core import *
 from aligo.core.Create import Create
@@ -17,7 +17,7 @@ class CustomShare(Core):
     ALIGO_SHARE_SCHEMA = 'aligo://'
 
     @staticmethod
-    def share_files_by_aligo(files: List[BaseFile]) -> str:
+    def __share_files_by_aligo(files: List[BaseFile]) -> List:
         """..."""
         result = []
         for file in files:
@@ -26,6 +26,12 @@ class CustomShare(Core):
                 'content_hash': file.content_hash,
                 'size': file.size
             })
+        return result
+
+    @staticmethod
+    def share_files_by_aligo(files: List[BaseFile]) -> str:
+        """..."""
+        result = CustomShare.__share_files_by_aligo(files)
         return CustomShare.ALIGO_SHARE_SCHEMA + base64.b64encode(json.dumps(result).encode()).decode()
 
     def __share_folder_by_aligo(self, parent_file_id: str, drive_id: str = None) -> List:
@@ -40,14 +46,38 @@ class CustomShare(Core):
                 continue
             # 3. 否则是文件夹, 递归
             result.append([file.name, self.__share_folder_by_aligo(parent_file_id=file.file_id, drive_id=drive_id)])
-        result.append(self.share_files_by_aligo(files))
+        result.append(self.__share_files_by_aligo(files))
         return result
 
     def share_folder_by_aligo(self, parent_file_id: str, drive_id: str = None) -> str:
         """..."""
-        return CustomShare.ALIGO_SHARE_SCHEMA + base64.b64encode(
-            json.dumps(
-                self.__share_folder_by_aligo(parent_file_id=parent_file_id, drive_id=drive_id)).encode()).decode()
+        result = self.__share_folder_by_aligo(parent_file_id=parent_file_id, drive_id=drive_id)
+        return CustomShare.ALIGO_SHARE_SCHEMA + base64.b64encode(json.dumps(result).encode()).decode()
+
+    def __save_files_by_aligo(self, data: str, parent_file_id: str = 'root',
+                              check_name_mode: CheckNameMode = 'auto_rename',
+                              drive_id: str = None):
+        result = []
+
+        # data 肯定是list, 内含 文件夹 list 或文件 dict
+        for obj in data:
+
+            if len(obj) > 0 and isinstance(obj[0], str):
+                # 创建文件夹
+                folder = Create.create_folder(self, CreateFolderRequest(name=obj[0], parent_file_id=parent_file_id,
+                                                                        drive_id=drive_id))
+                x = self.__save_files_by_aligo(obj[1], parent_file_id=folder.file_id)
+                result.append((folder, x))
+                continue
+
+            file: Dict
+            for file in obj:
+                x = self.create_by_hash(name=file['name'], content_hash=file['content_hash'], size=file['size'],
+                                        parent_file_id=parent_file_id, check_name_mode=check_name_mode,
+                                        drive_id=drive_id)
+                result.append(x)
+
+        return result
 
     def save_files_by_aligo(self, data: str, parent_file_id: str = 'root',
                             check_name_mode: CheckNameMode = 'auto_rename',
@@ -56,23 +86,9 @@ class CustomShare(Core):
         if not data.startswith(self.ALIGO_SHARE_SCHEMA):
             self._auth.log.warning(f'这不是合法 aligo 分享协议: {data}')
             return
-        result = []
+
         data = data[8:]
         data = json.loads(base64.b64decode(data))
-        for obj in data:
-            if isinstance(obj, list):
-                # 创建文件夹
-                folder = Create.create_folder(self, CreateFolderRequest(name=obj[0], parent_file_id=parent_file_id,
-                                                                        drive_id=drive_id))
-                x = self.save_files_by_aligo(obj[1], parent_file_id=folder.file_id)
-                result.append((folder, x))
-                continue
-            # 否则为 str, 内含文件列表
-            obj = obj[8:]
-            obj = json.loads(base64.b64decode(obj))
-            for file in obj:
-                x = self.create_by_hash(name=file['name'], content_hash=file['content_hash'], size=file['size'],
-                                        parent_file_id=parent_file_id, check_name_mode=check_name_mode,
-                                        drive_id=drive_id)
-                result.append(x)
+
+        result = self.__save_files_by_aligo(data, parent_file_id, check_name_mode, drive_id)
         return result
