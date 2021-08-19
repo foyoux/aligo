ea2089acff96c9ee21ed39c765fabab044e18f93
bob <1478449280@qq.com>
2021-08-19 09:47:31 +0800

    1.添加下载文件进度 2.优化文件名处理

--- aligo/core/Download.py
+++ aligo/core/Download.py
@@ -4,6 +4,7 @@ import re
 from typing import Iterator, List
 
 import requests
+from tqdm import tqdm
 
 from aligo.core import *
 from aligo.core.Config import *
@@ -14,7 +15,7 @@ from aligo.types import *
 
 class Download(BaseAligo):
     """..."""
-    CHUNK_SIZE = 10485760
+    CHUNK_SIZE = 8388608
 
     def get_download_url(self, body: GetDownloadUrlRequest) -> GetDownloadUrlResponse:
         """..."""
@@ -27,13 +28,13 @@ class Download(BaseAligo):
             body.drive_id = self.default_drive_id
 
         yield from self.batch_request(BatchRequest(
-                requests=[BatchSubRequest(
-                    id=file_id,
-                    url='/file/get_download_url',
-                    body=GetDownloadUrlRequest(
-                        drive_id=body.drive_id, file_id=file_id
-                    )
-                ) for file_id in body.file_id_list]
+            requests=[BatchSubRequest(
+                id=file_id,
+                url='/file/get_download_url',
+                body=GetDownloadUrlRequest(
+                    drive_id=body.drive_id, file_id=file_id
+                )
+            ) for file_id in body.file_id_list]
         ), GetDownloadUrlResponse)
 
     @staticmethod
@@ -42,8 +43,16 @@ class Download(BaseAligo):
         return re.sub(r'[\\/:*?"<>|]', '_', s)
 
     def download_file(self, file_path: str, url: str) -> str:
-        """下载文件"""
-        file_path = os.path.abspath(file_path)
+        """下载文件
+
+        :param file_path: 下载到哪里, 比如: 123.jpg, ./123.jpg, D:\123.jpg
+        :param url: 下载地址
+        :return: 下载完成保存文件的本地路径
+        """
+        file_dir, file_name = os.path.split(file_path)
+        file_name = self._del_special_symbol(file_name)
+        file_path = os.path.abspath(os.path.join(file_dir, file_name))
+
         # 递归创建目录
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
@@ -53,22 +62,35 @@ class Download(BaseAligo):
 
         tmp_file = file_path + '.ali'
 
+        self._auth.log.info(f'开始下载文件 {file_path}')
+
         with requests.get(url, headers={
             'referer': 'https://www.aliyundrive.com/'
         }, stream=True) as resp:
+            llen = int(resp.headers.get('content-length', 0))
+            progress_bar = tqdm(total=llen, unit='B', unit_scale=True)
             with open(tmp_file, 'wb') as f:
-                for chunk in resp.iter_content(chunk_size=int(Download.CHUNK_SIZE)):
-                    f.write(chunk)
+                for content in resp.iter_content(chunk_size=Download.CHUNK_SIZE):
+                    progress_bar.update(len(content))
+                    f.write(content)
 
         os.renames(tmp_file, file_path)
+
+        progress_bar.close()
+
         self._auth.log.info(f'文件下载完成 {file_path}')
         return file_path
 
     def download_files(self, files: List[BaseFile], local_folder: str = '.') -> List[str]:
-        """批量下载文件"""
+        """批量下载文件
+
+        :param files: BaseFile 对象列表, 或者包含 name 和  download_url 属性的对象
+        :param local_folder: 目标文件夹, 表示要下载到哪个文件夹
+        :return: 返回下载完成后的本地文件路径列表
+        """
         rt = []
         for file in files:
-            file_name = os.path.join(local_folder, self._del_special_symbol(file.name))
-            file_path = self.download_file(file_name, file.download_url)
+            file_path = os.path.join(local_folder, file.name)
+            file_path = self.download_file(file_path, file.download_url)
             rt.append(file_path)
         return rt
