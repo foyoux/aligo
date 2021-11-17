6f182530b6ef3f9f6eb78e5a25e429e1dee2a527
bob <1478449280@qq.com>
2021-11-17 21:20:09 +0800

    添加 get_path 文档注释和用法示例

--- aligo/apis/Other.py
+++ aligo/apis/Other.py
@@ -9,6 +9,19 @@ class Other(Core):
     """Other"""
 
     def get_path(self, file_id: str, drive_id: str = None) -> GetFilePathResponse:
-        """get_path: 获取文件的父级目录信息"""
+        """
+        官方：获取文件（夹）目录信息
+        :param file_id: 文件（夹）ID
+        :param drive_id: 文件（夹）所在网盘ID
+        :return: [GetFilePathResponse]
+
+        用法示例：
+        >>> from aligo import Aligo, BaseFile, GetFilePathResponse
+        >>> aligo = Aligo()
+        >>> file_path_info = aligo.get_path('60f927edf4c9f64d3a0c4704b80154cfa3d13c2a')
+        >>> assert isinstance(file_path_info, GetFilePathResponse)
+        >>> for item in file_path_info.items:
+        >>>     assert isinstance(item, BaseFile)
+        """
         body = GetFilePathRequest(file_id=file_id, drive_id=drive_id)
         return self._core_get_path(body)
