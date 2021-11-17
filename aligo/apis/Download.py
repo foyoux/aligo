abbdf0de9ef6890928352d854ba1a0b51e302d3c
bob <1478449280@qq.com>
2021-11-17 16:49:44 +0800

    测试并为 Download.py 添加文档注释和用法示例

--- aligo/apis/Download.py
+++ aligo/apis/Download.py
@@ -17,7 +17,20 @@ class Download(Core):
                          expire_sec: int = 14400,
                          drive_id: str = None,
                          ) -> GetDownloadUrlResponse:
-        """获取下载链接"""
+        """
+        获取下载链接: 一般在已知 file_id 或 download_url 失效时使用
+        :param file_id: [str] 文件 id
+        :param file_name: Optional[str] 文件名
+        :param expire_sec: Optional[int] 下载链接有效时间, 默认为 4 小时, 这也是允许的最大值
+        :param drive_id: Optional[str] 文件所在的网盘 id
+        :return: [GetDownloadUrlResponse]
+
+        用法示例:
+        >>> from aligo import Aligo
+        >>> ali = Aligo()
+        >>> result = ali.get_download_url(file_id='<file_id>')
+        >>> print(result)
+        """
         body = GetDownloadUrlRequest(
             file_id=file_id,
             drive_id=drive_id,
@@ -30,7 +43,19 @@ class Download(Core):
                            file_id_list: List[str],
                            expire_sec: int = 14400,
                            drive_id=None) -> List[BatchDownloadUrlResponse]:
-        """批量获取下载链接"""
+        """
+        批量获取下载链接
+        :param file_id_list: [List[str]] 文件 id 列表
+        :param expire_sec: [int] 下载链接有效时间, 默认为 4 小时, 这也是允许的最大值
+        :param drive_id: [str] 文件所在的网盘 id
+        :return: [List[BatchDownloadUrlResponse]]
+
+        用法示例:
+        >>> from aligo import Aligo
+        >>> ali = Aligo()
+        >>> result = ali.batch_download_url(file_id_list=['<file1_id>', '<file2_id>'])
+        >>> print(result)
+        """
         body = BatchDownloadUrlRequest(
             drive_id=drive_id,
             file_id_list=file_id_list,
@@ -40,7 +65,19 @@ class Download(Core):
         return [i for i in result]
 
     def download_folder(self, folder_file_id: str, local_folder: str = '.', drive_id: str = None) -> str:
-        """下载文件夹, 为解决先下载目标文件夹的问题"""
+        """
+        下载文件夹
+        :param folder_file_id: [str] 文件夹 id
+        :param local_folder: [str] 本地文件夹路径, 默认为当前目录, 即下载到哪里
+        :param drive_id: [str] 文件夹所在的网盘 id
+        :return: [str] 本地文件夹路径
+
+        用法示例:
+        >>> from aligo import Aligo
+        >>> ali = Aligo()
+        >>> result = ali.download_folder(folder_file_id='<folder_file_id>')
+        >>> print(result)
+        """
         if folder_file_id != 'root':
             folder = self._core_get_file(GetFileRequest(file_id=folder_file_id, drive_id=drive_id))
             local_folder = os.path.join(local_folder, self._del_special_symbol(folder.name))
@@ -62,19 +99,54 @@ class Download(Core):
 
     @overload
     def download_file(self, file_path: str, url: str) -> str:
-        """..."""
+        """
+        根据下载地址下载文件
+        :param file_path: [str] 文件路径
+        :param url: [str] 下载地址
+        :return: [str] 本地文件路径
+
+        用法示例:
+        >>> from aligo import Aligo
+        >>> ali = Aligo()
+        >>> result = ali.download_file(file_path='<file_path>', url='<url>')
+        >>> print(result)
+        """
 
     @overload
     def download_file(self, file_id: str, local_folder: str = '.') -> str:
-        """..."""
+        """
+        根据 file_id 下载文件
+        :param file_id: [str] 文件 id
+        :param local_folder: Optional[str] 本地文件夹路径, 默认为当前目录, 即下载到哪里
+        :return: [str] 本地文件路径
+
+        用法示例:
+        >>> from aligo import Aligo
+        >>> ali = Aligo()
+        >>> result = ali.download_file(file_id='<file_id>')
+        >>> print(result)
+        """
 
     @overload
     def download_file(self, file: BaseFile, local_folder: str = '.') -> str:
-        """..."""
+        """
+        根据 BaseFile 对象下载文件
+        :param file: [BaseFile] 文件对象
+        :param local_folder: Optional[str] 本地文件夹路径, 默认为当前目录, 即下载到哪里
+        :return: [str] 本地文件路径
+
+        用法示例:
+        >>> from aligo import Aligo
+        >>> ali = Aligo()
+        >>> # ll = ali.get_file_list()
+        >>> # result = ali.download_file(ll[-1])
+        >>> result = ali.download_file(file=BaseFile(file_id='<file_id>'))
+        >>> print(result)
+        """
 
     def download_file(self, file_path: str = None, url: str = None,
                       local_folder: str = '.', file_id: str = None, file: BaseFile = None, drive_id=None) -> str:
-        """..."""
+        """download_file"""
         if file_id:
             file = self._core_get_file(GetFileRequest(file_id=file_id, drive_id=drive_id))
 
