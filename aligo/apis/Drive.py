10a02e8b8da79118180f40087af2c53b073f64c6
bob <1478449280@qq.com>
2021-11-17 17:00:22 +0800

    测试并为 Download.py 添加文档注释和用法示例

--- aligo/apis/Drive.py
+++ aligo/apis/Drive.py
@@ -9,6 +9,16 @@ class Drive(Core):
     """..."""
 
     def get_drive(self, drive_id: str = None) -> BaseDrive:
-        """获取 drive"""
+        """
+        获取 drive 信息
+        :param drive_id: Optional[str] drive id，默认：None，如果为 None，则返回当前登录用户的 drive 信息
+        :return: [BaseDrive]
+
+        用法示例：
+        >>> from aligo import Aligo
+        >>> ali = Aligo()
+        >>> result = ali.get_drive()
+        >>> print(result)
+        """
         body = GetDriveRequest(drive_id=drive_id)
         return self._core_get_drive(body)
