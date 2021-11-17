68aa69c3d81571f1f11b33f0034f9399ccd2fecd
bob <1478449280@qq.com>
2021-11-17 14:47:59 +0800

    添加 get_audio_play_info 文档注释和用法示例

--- aligo/apis/Audio.py
+++ aligo/apis/Audio.py
@@ -1,4 +1,4 @@
-"""..."""
+"""Audio class"""
 
 from aligo.core import *
 from aligo.request import *
@@ -6,9 +6,20 @@ from aligo.response import *
 
 
 class Audio(Core):
-    """..."""
+    """音频相关"""
 
     def get_audio_play_info(self, file_id: str, drive_id: str = None) -> GetAudioPlayInfoResponse:
-        """获取音频播放信息"""
+        """
+        官方：获取音频播放信息
+        :param file_id: [str] 音频 file_id
+        :param drive_id: Optional[str] 音频文件的 drive_id
+        :return: [GetAudioPlayInfoResponse]
+
+        用法示例：
+        >>> from aligo import Aligo
+        >>> ali = Aligo()
+        >>> audio = ali.get_audio_play_info('<file_id>')
+        >>> print(audio)
+        """
         body = GetAudioPlayInfoRequest(file_id=file_id, drive_id=drive_id)
         return self._core_get_audio_play_info(body)
