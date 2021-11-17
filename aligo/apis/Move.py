f3443819187856aed99ddfe9b17168c14b2be768
bob <1478449280@qq.com>
2021-11-17 21:14:29 +0800

    测试并为 Move.py 添加文档注释和用法示例

--- aligo/apis/Move.py
+++ aligo/apis/Move.py
@@ -1,5 +1,5 @@
 """..."""
-from typing import List
+from typing import List, overload
 
 from aligo.core import *
 from aligo.request import *
@@ -9,6 +9,45 @@ from aligo.response import *
 class Move(Core):
     """..."""
 
+    @overload
+    def move_file(self, file_id: str,
+                  to_parent_file_id: str = 'root',
+                  new_name: str = None,
+                  drive_id: str = None,
+                  to_drive_id: str = None,
+                  **kwargs) -> MoveFileResponse:
+        """
+        移动文件
+        :param file_id: [必选] 文件ID
+        :param to_parent_file_id: [可选] 目标文件夹ID
+        :param new_name: [可选] 新文件名
+        :param drive_id: [可选] 文件所在的网盘ID
+        :param to_drive_id: [可选] 目标网盘ID
+        :param kwargs: [可选] 其他参数
+        :return: [MoveFileResponse]
+
+        用法示例：
+        >>> from aligo import Aligo, MoveFileRequest
+        >>> ali = Aligo()
+        >>> result = ali.move_file(file_id='<file_id>')  # 默认移动到 根目录
+        >>> print(result)
+        """
+
+    @overload
+    def move_file(self, body: MoveFileRequest) -> MoveFileResponse:
+        """
+        移动文件
+        :param body: [必选] 移动文件请求体
+        :return: [MoveFileResponse]
+
+        用法示例：
+        >>> from aligo import Aligo, MoveFileRequest
+        >>> ali = Aligo()
+        >>> body = MoveFileRequest(file_id='<file_id>')  # 默认移动到 根目录
+        >>> result = ali.move_file(body=body)
+        >>> print(result)
+        """
+
     def move_file(self, file_id: str = None,
                   to_parent_file_id: str = 'root',
                   new_name: str = None,
@@ -16,7 +55,7 @@ class Move(Core):
                   to_drive_id: str = None,
                   body: MoveFileRequest = None,
                   **kwargs) -> MoveFileResponse:
-        """移动文件"""
+        """move_file"""
         if body is None:
             body = MoveFileRequest(
                 file_id=file_id,
@@ -28,17 +67,56 @@ class Move(Core):
             )
         return self._core_move_file(body)
 
+    @overload
+    def batch_move_files(self,
+                         file_id_list: List[str],
+                         to_parent_file_id: str = 'root',
+                         drive_id: str = None,
+                         **kwargs) -> List[BatchSubResponse]:
+        """
+        批量移动文件
+        :param file_id_list: [必选] 文件ID列表
+        :param to_parent_file_id: [可选] 目标文件夹ID
+        :param drive_id: [可选] 文件所在的网盘ID
+        :param kwargs: [可选] 其他参数
+        :return: [List[BatchSubResponse]]
+
+        用法示例：
+        >>> from aligo import Aligo, BatchMoveFilesRequest
+        >>> ali = Aligo()
+        >>> file_id_list = ['<file1_id>', '<file2_id>']
+        >>> result = ali.batch_move_files(file_id_list=file_id_list)  # 默认移动到 根目录
+        >>> print(result)
+        """
+
+    @overload
+    def batch_move_files(self, body: BatchMoveFilesRequest) -> List[BatchSubResponse]:
+        """
+        批量移动文件
+        :param body: [必选] 批量移动文件请求体
+        :return: [List[BatchSubResponse]]
+
+        用法示例：
+        >>> from aligo import Aligo, BatchMoveFilesRequest
+        >>> ali = Aligo()
+        >>> body = BatchMoveFilesRequest(file_id_list=['<file1_id>', '<file2_id>'])  # 默认移动到 根目录
+        >>> result = ali.batch_move_files(body=body)
+        >>> print(result)
+        """
+
     def batch_move_files(self,
                          file_id_list: List[str] = None,
                          to_parent_file_id: str = 'root',
                          drive_id: str = None,
                          body: BatchMoveFilesRequest = None,
                          **kwargs) -> List[BatchSubResponse]:
-        """批量移动"""
+        """batch_move_files"""
         if body is None:
-            body = BatchMoveFilesRequest(drive_id=drive_id,
-                                         file_id_list=file_id_list,
-                                         to_parent_file_id=to_parent_file_id,
-                                         **kwargs)
+            body = BatchMoveFilesRequest(
+                drive_id=drive_id,
+                file_id_list=file_id_list,
+                to_parent_file_id=to_parent_file_id,
+                **kwargs
+            )
         result = self._core_batch_move_files(body)
         return [i for i in result]
