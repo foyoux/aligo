29ebd75994cb33349b0ba888e1ddb63e8cf9d78a
bob <1478449280@qq.com>
2021-11-19 21:14:56 +0800

    started notify

---
+++ .github/workflows/star_notify.py
@@ -0,0 +1,8 @@
+"""..."""
+import sys
+
+import yagmail
+
+if __name__ == '__main__':
+    yag = yagmail.SMTP(sys.argv[1], sys.argv[2], 'smtp.qq.com', 465)
+    yag.send(sys.argv[3], 'github aligo started', 'github aligo started')
