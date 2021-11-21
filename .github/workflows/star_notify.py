7aad212f0d1ebd61e7ca2039e74a716dd9d85a96
bob <1478449280@qq.com>
2021-11-21 21:29:02 +0800

    update notify

--- .github/workflows/star_notify.py
+++ .github/workflows/star_notify.py
@@ -1,8 +1,27 @@
 """..."""
 import sys
 
+import requests
 import yagmail
 
 if __name__ == '__main__':
-    yag = yagmail.SMTP(sys.argv[1], sys.argv[2], 'smtp.qq.com', 465)
-    yag.send(sys.argv[3], 'github aligo started', 'github aligo started\n' + sys.argv[4])
+    print('参数数量', len(sys.argv))
+    data = requests.post(
+        'https://api.github.com/graphql',
+        json={
+            'query': """{ repository(name: "aligo", owner: "foyoux") { stargazerCount stargazers(last: 1) { edges { node { name url avatarUrl email } } } } }"""},
+        headers={
+            'Authorization': 'Bearer ' + sys.argv[1],
+        }
+    ).json()['data']
+    last = data['repository']['stargazers']['edges'][0]
+    yag = yagmail.SMTP(sys.argv[2], sys.argv[3], 'smtp.qq.com', 465)
+    yag.send(sys.argv[4], 'aligo 项目新增 star', f"""
+    <div>
+        <h1>aligo 项目新增 star</h1>
+        <h2>当前总 star {data['repository']['stargazerCount']}</h2>
+        <h3>最新 star 用户 {last['name']}</h3>
+        <h4>最新 star 用户 repo {last['url']}</h3>
+        <img src="{last['avatarUrl']}" />
+    </div>
+    """)
