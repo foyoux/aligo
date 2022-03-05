53c5d3f43e4dd995b6777b3d35e2dd797661be5a
foyou <yimi.0822@qq.com>
2022-03-05 17:54:09 +0800

    添加外层注释

--- aligo/apis/Aligo.py
+++ aligo/apis/Aligo.py
@@ -1,4 +1,9 @@
 """Aligo class"""
+import logging
+from typing import Callable, Dict
+
+from typing_extensions import NoReturn
+
 from .Audio import Audio
 from .Copy import Copy
 from .Create import Create
@@ -36,3 +41,25 @@ class Aligo(
     SyncFolder,
 ):
     """阿里云盘"""
+
+    def __init__(
+            self,
+            name: str = 'aligo',
+            refresh_token: str = None,
+            show: Callable[[str], NoReturn] = None,
+            level: int = logging.DEBUG,
+            loglog: bool = False,
+            use_aria2: bool = False,
+            proxies: Dict = None
+    ):
+        """
+        Aligo
+        :param name: (可选, 默认: aligo) 配置文件名称, 便于使用不同配置文件进行身份验证
+        :param refresh_token:
+        :param show: (可选) 显示二维码的函数
+        :param level: (可选) 控制控制台输出
+        :param loglog: (可选) 控制文件输出
+        :param use_aria2: [bool] 是否使用 aria2 下载
+        :param proxies: (可选) 自定义代理 [proxies={"https":"localhost:10809"}],支持 http 和 socks5（具体参考requests库的用法）
+        """
+        super().__init__(name, refresh_token, show, level, loglog, use_aria2, proxies)
