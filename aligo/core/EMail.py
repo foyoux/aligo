b052e646f1c48662abff9f0988be2af40f603964
foyou <yimi.0822@qq.com>
2022-03-10 16:12:53 +0800

    update EMail.py

--- aligo/core/EMail.py
+++ aligo/core/EMail.py
@@ -7,19 +7,19 @@ from email.mime.text import MIMEText
 from email.utils import formataddr
 
 
-def send_email(recevier: str, title: str, content: str, qr_data: bytes):
+def send_email(receiver: str, title: str, content: str, qr_data: bytes):
     """发送邮件"""
     sender = 'aligo_notify@163.com'
 
     msg_root = MIMEMultipart()
     msg_root['From'] = formataddr(('aligo notify', sender))
-    msg_root['To'] = formataddr((recevier, recevier))
+    msg_root['To'] = formataddr((receiver, receiver))
     msg_root['Subject'] = f'[阿里云盘/{title}] 扫码登录'
 
     msg_root.attach(
         MIMEText(f'<div align="center"><h3>{content}</h3><img style="max-width: 100%" src="cid:qrcode"></div>', 'html'))
 
-    msg_image = MIMEImage(qr_data, )
+    msg_image = MIMEImage(qr_data)
     msg_image.add_header('Content-ID', '<qrcode>')
 
     msg_root.attach(msg_image)
@@ -29,11 +29,11 @@ def send_email(recevier: str, title: str, content: str, qr_data: bytes):
     for i in range(1, 4):
         try:
             result = smtp.sendmail(
-                'aligo_notify@163.com',
-                [recevier],
+                sender,
+                [receiver],
                 msg_root.as_bytes()
             )
             return result
         except smtplib.SMTPServerDisconnected as e:
             time.sleep(i * 3)
-    assert False, f'邮件发送失败 {recevier}'
+    assert False, f'邮件发送失败 {receiver}'
