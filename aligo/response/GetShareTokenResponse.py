dc8879d6c87cd0e81fb9bb2b7a88eb74b5d01dfc
foyou <yimi.0822@qq.com>
2022-11-15 20:27:59 +0800

    fix #55
    临时修补方案

--- aligo/response/GetShareTokenResponse.py
+++ aligo/response/GetShareTokenResponse.py
@@ -7,16 +7,15 @@ from aligo.types import DataClass
 @dataclass
 class GetShareTokenResponse(DataClass, str):
     """..."""
+    share_token: str = None
+    expire_time: str = None
+    expires_in: int = None
 
     def __new__(cls, *args, **kwargs):
         share_token = kwargs.get('share_token')
         if share_token is None:
             share_token = args[0]
         return super().__new__(cls, share_token)
-
-    share_token: str = None
-    expire_time: str = None
-    expires_in: int = None
 
     def __str__(self):
         return self.share_token
