b6e62d898038526534d4b157863a80f637fa3143
foyou <yimi.0822@qq.com>
2023-02-04 14:02:21 +0800

    fix m3u8 expire 15 after minutes

--- aligo/request/GetVideoPreviewPlayInfoRequest.py
+++ aligo/request/GetVideoPreviewPlayInfoRequest.py
@@ -12,3 +12,4 @@ class GetVideoPreviewPlayInfoRequest(DataClass):
     drive_id: str = None
     category: GetVideoPreviewCategory = 'live_transcoding'
     template_id: VideoTemplateID = None
+    url_expire_sec: int = 14400
