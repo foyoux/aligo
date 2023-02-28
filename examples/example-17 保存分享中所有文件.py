5a886d79bcff4244e50fc4ba16f172f7c8b7eed4
foyou <yimi.0822@qq.com>
2023-02-28 11:17:15 +0800

    fix #89

--- examples/example-17 保存分享中所有文件.py
+++ examples/example-17 保存分享中所有文件.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     # 特别说明
     # 如果遇到分享文件非常多，此段代码运行完成后，不会立马看到所有文件，可能需要几个小时才能陆续保存完成
     # 在网页保存需要几个小时，用这个一下就可以了，阿里云服务器会处理，不用等待
-    ali.share_file_save_all_to_drive(share_id, share_token)
+    ali.share_file_save_all_to_drive(share_token)
