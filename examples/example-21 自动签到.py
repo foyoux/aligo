963ea8311dee390e9e3f6ed4f253d5be647ae49b
foyou <yimi.0822@qq.com>
2023-04-24 10:49:22 +0800

    1

--- examples/example-21 自动签到.py
+++ examples/example-21 自动签到.py
@@ -6,15 +6,15 @@ class CAligo(Aligo):
         return self._post(
             '/v1/activity/sign_in_list',
             host='https://member.aliyundrive.com',
-            body={'isReward': False},
+            body={'isReward': True},
             params={'_rx-s': 'mobile'}
         )
 
-    def sign_in_reward(self):
+    def sign_in_reward(self, day):
         return self._post(
             '/v1/activity/sign_in_reward',
             host='https://member.aliyundrive.com',
-            body={'signInDay': 1},
+            body={'signInDay': day},
             params={'_rx-s': 'mobile'}
         )
 
@@ -32,12 +32,12 @@ if __name__ == '__main__':
     # noinspection PyProtectedMember
     log = ali._auth.log
 
-    # 签到
-    resp = ali.sign_in_reward()
-    log.info(resp.json()['result']['notice'])
-
     # 获取签到列表
     resp = ali.sign_in_list()
     result = resp.json()['result']
     signInCount = result['signInCount']
     log.info("本月签到次数: %d", signInCount)
+
+    # 签到
+    resp = ali.sign_in_reward(signInCount)
+    log.info(resp.json()['result']['notice'])
