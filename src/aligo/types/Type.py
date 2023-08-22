6e4bba7eabf5473a66a4c9986c2186930aae8269
foyou <yimi.0822@qq.com>
2023-08-22 10:12:22 +0800

    微调代码

--- src/aligo/types/Type.py
+++ src/aligo/types/Type.py
@@ -2,5 +2,5 @@ from typing import TypeVar
 
 from datclass import get_datclass
 
-DataType = TypeVar('DataType')  # 泛型类型变量
 DatClass = get_datclass(log=False)  # 使 dataclass 支持嵌套 和 扩展字段
+DataType = TypeVar('DataType', bound=DatClass)  # 泛型类型变量
