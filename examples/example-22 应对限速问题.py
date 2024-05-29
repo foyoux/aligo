0c7a9097401ffcbee17c287a8d50c7601890a562
foyou <yimi.0822@qq.com>
2024-05-29 20:28:51 +0800

    update idm download

--- examples/example-22 应对限速问题.py
+++ examples/example-22 应对限速问题.py
@@ -1,27 +1,12 @@
-"""
-使用多线程下载工具可以解决限速问题，作者常用下载工具是：Internet Download Manager
-
-对于下载单个文件，或者文件夹下的文件（不包括子目录），可以通过 get_file_list 函数获取文件列表
-
-然后打印文件下载链接，复制（可以包含其他字符串，前后空白等，IDM 会自动提取链接）到 IDM 中，
-在菜单中选择任务->从剪贴板中批量下载，然后选择下载到单独目录即可
-
-但是对于子目录，这种方式并不适用，因为文件夹路径不同，IDM 无法自动创建子目录
-
-这里提供一个例子，使用 IDM 命令行参数进行调用，并且会依次创建子目录
-"""
 import logging
 import os
 import re
+import subprocess
 from pathlib import Path
 
-from aligo import Aligo, BaseFile
+import psutil
 
-# 本地目录，用于存放下载的文件夹
-download_path = Path.home() / 'Downloads/AliyunDrive/'
-
-# IDM 主程序路径
-idm = 'C:\\MyProgram\\IDM\\IDMan.exe'
+from aligo import Aligo, BaseFile, Null
 
 
 def del_special_symbol(s: str) -> str:
@@ -29,35 +14,72 @@ def del_special_symbol(s: str) -> str:
     return re.sub(r'[:*?"<>|]', '_', s)
 
 
-def callback(file_path: str, file: BaseFile):
-    file.name = del_special_symbol(file.name)
-    file_path = del_special_symbol(file_path)
-    (download_path / file_path).mkdir(parents=True, exist_ok=True)
-    # print(f'"{idm}" /a /n /d "{file.download_url}" /p "{download_path / file_path}" /f "{file.name}"')
-    cmd = f'{idm} /a /n /d "{file.download_url or file.url}" /p "{download_path / file_path}" /f "{file.name}"'
-    print(cmd)
-    if os.path.exists(idm.replace('"', '')):
-        os.system(cmd)
-        os.system(f'{idm} /s')
+def download_by_idm(parent_file_id: str, drive_id: str = None, download_path: str = None):
+    """使用 IDM 下载文件（夹）
 
+    :param parent_file_id: 阿里云盘文件（夹）ID
+    :param drive_id: drive_id, 默认 备份盘，res 为资源盘
+    :param download_path: 下载路径，默认用户下载目录下 AliyunDrive 文件夹
+    :return:
+    """
+    a = [i.exe() for i in psutil.process_iter() if i.name() == 'IDMan.exe']
+    if a:
+        idm: str = a[0]
+    else:
+        print('IDM 未运行')
+        exit(1)
 
-def main():
-    global download_path
-    # noinspection SpellCheckingInspection
-    os.system('chcp 65001')
+    if download_path is None:
+        download_path = Path.home() / 'Downloads/AliyunDrive/'
+    else:
+        download_path = Path(download_path)
 
     ali = Aligo(level=logging.ERROR)
 
-    # 必须是一个文件夹
-    drive_id = ali.v2_user_get().resource_drive_id
-    parent_file_id = '64dcd9b5356a247012a44206a6dca0b5ab304c8e'
+    # 处理 drive_id
+    if drive_id == 'res':
+        drive_id = ali.v2_user_get().resource_drive_id
 
     # 创建 parent_file_id 文件夹
-    folder = ali.get_file(file_id=parent_file_id, drive_id=drive_id)
-    download_path = download_path / del_special_symbol(folder.name)
+    file_or_folder = ali.get_file(file_id=parent_file_id, drive_id=drive_id)
+    if isinstance(file_or_folder, Null):
+        print('获取文件失败', file_or_folder.message)
+        exit(1)
 
+    def callback(file_path: str, file: BaseFile):
+        file.name = del_special_symbol(file.name)
+        file_path = del_special_symbol(file_path)
+        (download_path / file_path).mkdir(parents=True, exist_ok=True)
+        if file.type == 'folder':
+            return
+        url = file.download_url or file.url
+        if not url:
+            dd = ali.get_download_url(file_id=file.file_id, drive_id=file.drive_id)
+            if not dd.url:
+                print('无法下载', file.name, file.file_id, file.drive_id, dd)
+                return
+            else:
+                url = dd.url
+        cmd = [idm, '/a', '/n', '/d', url, '/p', download_path / file_path, '/f', file.name]
+        _f = download_path / file_path / file.name
+        if _f.exists():
+            print(f'{_f} 已存在, 跳过下载')
+            return
+        print(cmd)
+        if os.path.exists(idm.replace('"', '')):
+            subprocess.run(cmd)
+            subprocess.run([idm, '/s'])
+
+    if file_or_folder.type == 'file':
+        callback('', file_or_folder)
+        exit(0)
+
+    download_path = download_path / del_special_symbol(file_or_folder.name)
+    os.system('chcp 65001')
     ali.walk_files(callback, parent_file_id=parent_file_id, drive_id=drive_id)
+
+    return 'Done!'
 
 
 if __name__ == '__main__':
-    main()
+    download_by_idm('folder-xxx')
